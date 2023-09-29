91692fea65ce06e0191ee73ce276abf67807fe0e
Dani Torres <danitor1@hotmail.com>
2023-09-30 00:33:06 +0200

    Structure of Task Master X

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TodayComponent } from './components/home/today/today.component';
+import { CalendarComponent } from './components/home/calendar/calendar.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { HeaderComponent } from './components/home/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TodayComponent,
+    CalendarComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
